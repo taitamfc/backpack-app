f87885bd49e83a908902e148e9483eb1d8cacb0e
taitamfc <taitam8590@gmail.com>
2022-11-12 14:45:57 +0700

    add title and meta in Return events + category api

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -10,6 +10,14 @@ class CategoryController extends Controller
 {
     public function index(){
         $items = Category::all();
+        foreach ($items as $item) {
+            if( $item->seo_title == '' ){
+                $item->seo_title = config('settings.product_cat_seo_title');
+            }
+            if( $item->seo_description == '' ){
+                $item->seo_description = config('settings.product_cat_meta_description');
+            }
+        }
         return response()->json($items);
     }
 }
--- app/Http/Controllers/Api/EventController.php
+++ app/Http/Controllers/Api/EventController.php
@@ -10,6 +10,14 @@ class EventController extends Controller
 {
     public function index(){
         $items = Event::all();
+        foreach ($items as $item) {
+            if( $item->seo_title == '' ){
+                $item->seo_title = config('settings.product_event_seo_title');
+            }
+            if( $item->seo_description == '' ){
+                $item->seo_description = config('settings.product_event_meta_description');
+            }
+        }
         return response()->json($items);
     }
 }
